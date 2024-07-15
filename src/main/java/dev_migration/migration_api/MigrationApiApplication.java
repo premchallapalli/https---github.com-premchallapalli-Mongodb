1b492d3959e73d06f718ae547615c0db462f0cdf
prem challapalli <68176640+premchallapalli@users.noreply.github.com>
2024-07-15 13:39:09 -0700

    first commit

---
+++ src/main/java/dev_migration/migration_api/MigrationApiApplication.java
@@ -0,0 +1,13 @@
+package dev_migration.migration_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev_migration/migration_api/MigrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev_migration.migration_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
